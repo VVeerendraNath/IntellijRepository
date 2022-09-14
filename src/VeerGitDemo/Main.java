00085370d22ad7e22127b41c8b46a32480a9fa16
Veerendra <veeren.velupuri@gmail.com>
2022-09-14 19:39:16 +0530

    First Branch

--- src/VeerGitDemo/Main.java
+++ src/VeerGitDemo/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Direct commit and Push");
+        System.out.println("This is First Branch");
     }
 }
